99847402b1144aa83dd7274697fefafa0bcb92c4
João Gabriel <joao.florv2005@gmail.com>
2024-04-24 11:23:09 -0300

    Integracao feita

---
+++ ChatAppReflex/state.py
@@ -0,0 +1,35 @@
+import os
+
+from openai import AsyncOpenAI
+
+import reflex as rx
+
+class State(rx.State):
+    question: str
+
+    chat_history: list[tuple[str, str]]
+
+    async def answer(self):
+        client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        session = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": self.question}],
+            stop=None,
+            temperature=0.7,
+            stream=True,
+        )
+
+        answer = ""
+        self.chat_history.append((self.question, answer))
+
+        self.question = ""
+
+        yield
+
+        async for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                if item.choices[0].delta.content is None:
+                    break
+                answer += item.choices[0].delta.content
+                self.chat_history[-1] = (self.chat_history[-1][0], answer)
+                yield
---
+++ README.md
@@ -0,0 +1,9 @@
+# Configuração do Aplicativo
+
+Para testar o app, você precisa da sua API_KEY da OpenAI. 
+
+1. Entra no site e cria a conta ou faz login (https://openai.com).
+2. Vai na seção de gerenciamento de API e cria a chave nova.
+3. Na sua máquina, cria a variável de ambiente `OPENAI_API_KEY` e cola a chave que você gerou.
+
+Depois disso já deve tá pegando normalmente.
